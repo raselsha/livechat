58fdcc8ca2aef29dfe9dc33e5519f1299e0eb985
raselsha <raselsha@gmail.com>
2021-03-06 02:16:39 +0000

    all files added to upload

---
+++ php/config/parameters.php
@@ -0,0 +1,34 @@
+<?php
+
+return array (
+  'dbHost' => 'localhost',
+  'dbPort' => '3306',
+  'dbUser' => 'dontobkg_insura',
+  'dbPassword' => 'one1two2',
+  'dbName' => 'dontobkg_livechat',
+  'superUser' => 'admin',
+  'superPass' => 'one1two2',
+  'services' => 
+  array (
+    'verify' => 
+    array (
+      'code' => 'cc0520cb-d2fb-4f4b-9902-9639687a6387',
+      'token' => 'df5373a31ad374d359d1dd22bec41a9f76c359aa',
+    ),
+    'mailer' => 
+    array (
+      'smtp' => '',
+      'smtpSecure' => 'ssl',
+      'smtpHost' => '',
+      'smtpPort' => '465',
+      'smtpUser' => '',
+      'smtpPass' => '',
+    ),
+  ),
+  'appSettings' => 
+  array (
+    'contactMail' => 'raselsha@gmail.com',
+  ),
+);
+
+?>
